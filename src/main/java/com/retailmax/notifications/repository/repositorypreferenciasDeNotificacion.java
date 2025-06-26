6416b5c0be15a743692a03defc9c925c695d246a
Sebastianrg <seba.riquelmeg@duocuc.cl>
2025-06-26 01:24:28 -0400

    Revert "Subiendo cambios oficiales de RF-05.2"
    This reverts commit d05da6d45540d64edcc3719b684c389dafa45d84.

--- src/main/java/com/retailmax/notifications/repository/repositorypreferenciasDeNotificacion.java
+++
@@ -1,9 +0,0 @@
-package com.retailmax.notifications.repository;
-
-import com.retailmax.notifications.model.PreferenciasDeNotificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RepositoryPreferenciasDeNotificacion extends JpaRepository<PreferenciasDeNotificacion, Long> {
-    List<PreferenciasDeNotificacion> findByClienteId(Long clienteId);
-}
